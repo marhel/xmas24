9cfba7a39b8bd71102ed424174affb367c118a2b
Martin Hellspong <martin.hellspong@factor10.com>
2024-11-21 21:08:37 +0100

    This year is green

--- docs/local-web.sh
+++ docs/local-web.sh
@@ -1,1 +1,1 @@
-miniserve --route-prefix xmas23 --index index.html --interfaces 127.0.0.1 --interfaces 192.168.50.158 --qrcode .
+miniserve --route-prefix xmas --index index.html --interfaces 127.0.0.1 --interfaces 192.168.50.158 --qrcode .
